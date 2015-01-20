5cf17f9541c18b03dc66e71bd017ef4de02d2d27
exibit <benoit@exibit.be>
2015-01-20 10:52:00 +0100

    Delete my own Instagram Token and ID

---
+++ js/main.js
@@ -0,0 +1,11 @@
+jQuery(document).ready(function ($) {
+
+	/* INSTASTREAM DEMO */
+  $("#demo1").instastream({
+		instaToken: 'Your Instagram Token here',// More information on http://instagram.com/developer/
+		instaUser: 'Your Instagram User ID here',//
+		instaResults: 3,
+		instaMenu: 'yes'
+	});
+	
+});
